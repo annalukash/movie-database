2e68b0be733685907ef50cfa3d8cb504ca047ad5
Anna <anna.lukash68@gmail.com>
2020-08-31 16:06:11 +0300

    Updates

---
+++ precache-manifest.2d0d63c761ec91de31d55f7d74f93311.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7cb24f830b21b7f0372b57675dd6484",
+    "url": "/movie-database/index.html"
+  },
+  {
+    "revision": "47c87f2c46bca43d5256",
+    "url": "/movie-database/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "aa667d6a1ebac58f3ba9",
+    "url": "/movie-database/static/css/main.9368269c.chunk.css"
+  },
+  {
+    "revision": "47c87f2c46bca43d5256",
+    "url": "/movie-database/static/js/2.aae30ff5.chunk.js"
+  },
+  {
+    "revision": "050bfd46aff4ace3ad398173b3e2b481",
+    "url": "/movie-database/static/js/2.aae30ff5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa667d6a1ebac58f3ba9",
+    "url": "/movie-database/static/js/main.30454074.chunk.js"
+  },
+  {
+    "revision": "d06578ea575fb628d8aa",
+    "url": "/movie-database/static/js/runtime-main.e4b185c9.js"
+  }
+]);
